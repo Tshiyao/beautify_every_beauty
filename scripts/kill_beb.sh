496287c62d1d2b9fa082979ddca5c5f161b0e311
Tshiyao <1524072954@qq.com>
2024-12-14 20:55:28 +0800

    bash

--- scripts/kill_beb.sh
+++ scripts/kill_beb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # 定义要查找和杀死的进程关键字
-process_keyword="python -m vllm"
+process_keyword="vllm"
 # 使用pgrep命令查找进程的PID（进程标识符）
-pids=$(pgrep -f $process_keyword)
+pids=$(pgrep -f "$process_keyword")
 if [ -n "$pids" ]; then
   # 如果找到进程，循环遍历每个PID并杀死进程
   for pid in $pids; do
@@ -11,4 +11,4 @@ if [ -n "$pids" ]; then
   done
 else
   echo "未找到包含关键字 $process_keyword 的进程。"
-end
+fi
